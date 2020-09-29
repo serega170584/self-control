3c49ad8df6efd5f1372811f4d03bf89dced92e67
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-09-29 09:41:36 +0300

    add kruskal algorithm

--- kruskal-mst.php
+++ kruskal-mst.php
@@ -1,5 +1,6 @@
 <?php
 $count = rand(5, 15);
+$graph = [];
 for ($i = 0; $i < $count; ++$i) {
     for ($j = $i; $j < $count; ++$j) {
         $randInd = rand(0, $count - 1);
@@ -9,12 +10,12 @@ for ($i = 0; $i < $count; ++$i) {
 }
 $edges = getEdges($graph);
 
+
 $parentList = range(0, $count - 1);
 $countList = array_fill(0, $count, 1);
 $mst = [];
 while ($list = current($edges)) {
-    $weight = key($list);
-    var_dump($weight);
+    $weight = key($edges);
     while ($edge = current($list)) {
         if (!isConnected($parentList, $edge[0], $edge[1])) {
             connect($parentList, $edge[0], $edge[1], $countList);
@@ -24,6 +25,7 @@ while ($list = current($edges)) {
     }
     next($edges);
 }
+var_dump($count);
 var_dump($mst);
 die('asd');
 
@@ -58,10 +60,10 @@ function connect(&$parentList, $i, $j, &$countList)
     $iRootCount = $countList[$iRoot];
     $jRootCount = $countList[$jRoot];
     if ($iRootCount > $jRootCount) {
-        $parentList[$j] = $i;
+        $parentList[$jRoot] = $iRoot;
         $countList[$iRoot] += $jRootCount;
     } else {
-        $parentList[$i] = $j;
+        $parentList[$iRoot] = $jRoot;
         $countList[$jRoot] += $iRootCount;
     }
 }
