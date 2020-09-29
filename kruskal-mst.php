30777bc446cc820cdf0819b4dfd83f28cd384f45
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-09-29 08:37:43 +0300

    add prima mst alogorithm

--- kruskal-mst.php
+++ kruskal-mst.php
@@ -1,4 +1,48 @@
 <?php
+$count = rand(5, 15);
+for ($i = 0; $i < $count; ++$i) {
+    for ($j = $i; $j < $count; ++$j) {
+        $randInd = rand(0, $count - 1);
+        $graph[$randInd][$i] = $graph[$i][$randInd] = rand(1, 1000);
+    }
+    unset($graph[$i][$i]);
+}
+$edges = getEdges($graph);
+
+$parentList = range(0, $count - 1);
+$countList = array_fill(0, $count, 1);
+$mst = [];
+while ($list = current($edges)) {
+    $weight = key($list);
+    var_dump($weight);
+    while ($edge = current($list)) {
+        if (!isConnected($parentList, $edge[0], $edge[1])) {
+            connect($parentList, $edge[0], $edge[1], $countList);
+            $mst[$edge[0]][$edge[1]] = $weight;
+        }
+        next($list);
+    }
+    next($edges);
+}
+var_dump($mst);
+die('asd');
+
+function getEdges($graph)
+{
+    $count = count($graph);
+    $edges = [];
+    for ($i = 0; $i < $count; ++$i) {
+        $weights = $graph[$i];
+        while ($weight = current($weights)) {
+            $vertex = key($weights);
+            $edges[$weight][] = [$i, $vertex];
+            next($weights);
+        }
+    }
+    ksort($edges);
+    return $edges;
+}
+
 function getRoot($parentList, $i)
 {
     while ($parentList[$i] != $i) {
@@ -16,16 +60,14 @@ function connect(&$parentList, $i, $j, &$countList)
     if ($iRootCount > $jRootCount) {
         $parentList[$j] = $i;
         $countList[$iRoot] += $jRootCount;
+    } else {
+        $parentList[$i] = $j;
+        $countList[$jRoot] += $iRootCount;
     }
 }
 
-$count = rand(5, 15);
-for ($i = 0; $i < $count; ++$i) {
-    for ($j = $i; $j < $count; ++$j) {
-        $randInd = rand(0, $count - 1);
-        $graph[$randInd][$i] = $graph[$i][$randInd] = rand(1, 1000);
-    }
-    unset($graph[$i][$i]);
+function isConnected($parentList, $i, $j)
+{
+    return getRoot($parentList, $i) == getRoot($parentList, $j);
 }
-var_dump($graph);
-die('asd');
+
