e53832d74978e82236a1466be0506cf790ba8a2b
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-10-12 17:01:24 +0300

    add msd new time control

---
+++ msd-new-time-control.php
@@ -0,0 +1,54 @@
+<?php
+function charAt($str, $i)
+{
+    return $str[$i] ?? '';
+}
+
+$count = rand(5, 15);
+$maxLength = 0;
+$list = [];
+$seq = 'abcdefghijklmnopqrstuvwxyz';
+for ($i = 0; $i < $count; ++$i) {
+    $length = rand(5, 15);
+    $maxLength = $maxLength > $length ? $maxLength : $length;
+    $list[] = substr($seq, rand(5, 15), $length);
+}
+$queue = [[0, $count]];
+$pos = 0;
+while ($pos < $maxLength) {
+    $res = [];
+    $itQueue = [];
+    while ($queue) {
+        $el = array_shift($queue);
+        $fromIndex = $el[0];
+        $toCount = $el[1];
+        $counts = [];
+        for ($i = $fromIndex; $i < $toCount; ++$i) {
+            $char = charAt($list[$i], $pos);
+            $code = ord($char);
+            $counts[$code] = $counts[$code] ?? 0;
+            ++$counts[$code];
+        }
+        ksort($counts);
+        $curCount = 0;
+        while ($curItCount = current($counts)) {
+            $code = key($counts);
+            $counts[$code] = $curCount;
+            $curCount += $curItCount;
+            next($counts);
+        }
+        $fromIndexes = $counts;
+        for ($i = $fromIndex; $i < $toCount; ++$i) {
+            $char = charAt($list[$i], $pos);
+            $code = ord($char);
+            $res[$fromIndex + $counts[$code]++] = $list[$i];
+        }
+        foreach ($fromIndexes as $code => $itCount) {
+            $itQueue[] = [$fromIndex + $itCount, $fromIndex + $counts[$code]];
+        }
+    }
+    $list = $res;
+    ++$pos;
+    $queue = $itQueue;
+}
+var_dump($list);
